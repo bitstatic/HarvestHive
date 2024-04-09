1f6b88e1865d8ed3c8faf49bffd93cbd16eacd5a
sukumar1210 <sukumar.rtg@gmail.com>
2024-04-09 13:51:13 +0530

    setup Initial MUI config

---
+++ src/theme.ts
@@ -0,0 +1,37 @@
+'use client';
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#67A438',
+    },
+    secondary: {
+      main: '#B3E271',
+      contrastText: '#0A2919',
+    },
+    error: {
+      main: '#C63939',
+    },
+    warning: {
+      main: '#D77C28',
+    },
+    info: {
+      main: '#3D6CC7',
+    },
+    success: {
+      main: '#7ECE40',
+      contrastText: '#0A2919',
+    },
+    background: {
+      default: '#E6FDF0',
+      paper: '#F4FEF8',
+    },
+    text: {
+      primary: '#071D12',
+    },
+  },
+})
+
+export default theme;
