db496f3693c2f27fba585f77d3a8aa5f63b59cea
Vineet Anil Likhitkar <vind3v17@gmail.com>
2024-04-28 04:48:00 +0530

    [Dev] Created Data Models for Entities

---
+++ src/models/PaymentModel.ts
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+// import uniqueValidator from 'mongoose-unique-validator';
+
+const PaymentSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    order: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Order',
+        required: true
+    },
+    amount: {
+        type: Number,
+        required: true
+    },
+    paymentMethod: {
+        type: String,
+        enum: ['credit_card', 'paypal', 'cash_on_delivery'],
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['pending', 'completed', 'failed'],
+        default: 'pending'
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+// PaymentSchema.plugin(uniqueValidator);
+
+const PaymentModel = mongoose.model('Payment', PaymentSchema);
+
+module.exports = PaymentModel;
