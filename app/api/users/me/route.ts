1323763df6463115e9c9e4f7d3922348239513fe
Vineet Anil Likhitkar <vind3v17@gmail.com>
2024-05-03 01:20:14 +0530

    [DEV] API User Data | Setting Session Cookie

---
+++ app/api/users/me/route.ts
@@ -0,0 +1,43 @@
+import { connectToDB } from '@/database/dbConfig/dbConfig'
+import User from '@/database/models/userModel';
+import { NextRequest, NextResponse } from 'next/server';
+import { decrypt } from '@/src/util/session';
+
+connectToDB();
+
+export async function POST(request: NextRequest) {
+
+    try {
+        // Extract Data from Token
+        const token = request.cookies.get("token")?.value || "";
+
+        const SessionPayload:any = await decrypt(token).then((data)=>{return data});
+
+        console.log("Session Payload: ");
+        console.log(SessionPayload) ;
+        
+
+        const userId = SessionPayload.id;
+
+        console.log("User ID: " + userId);
+
+        const user = await User.findById(userId).select("-password -emailVerificationToken -emailVerificationTokenSentAt -emailConfirmTokenSentCount -emailVerified -emailVerifiedAt -verifyEmailToken -verifyEmailTokenExpiry -isVerified -createdAt -updatedAt");
+        
+        if(!user) {
+            return NextResponse.json({ error: "User not found" }, { status: 400, statusText: "Not Found"});
+        }
+
+        return NextResponse.json({ user: user }, { status: 200, statusText: "OK"});
+
+
+    } catch (error: any) {
+        return NextResponse.json({
+            error: error.message
+        }, {
+            status: 500,
+            statusText: "Internal Server Error"
+        });
+    }
+
+
+}
