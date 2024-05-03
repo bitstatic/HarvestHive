0d37d68f9832e58ed8e7db49f5cdc72ba8e22450
Dev jaiswal <devj8496@gmail.com>
2024-05-03 10:40:43 +0530

    added modal window in /f/listings/add

--- app/f/listings/add/page.tsx
+++ app/f/listings/add/page.tsx
@@ -3,19 +3,22 @@ import AppbarWMenu from '@/src/components/appbars/AppbarWMenu'
 import Logo from '@/src/components/Logo'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Payment } from '@mui/icons-material'
-import { Box, Button, FormControl, InputAdornment, Link, Paper, RadioGroup, TextField, Typography } from '@mui/material'
+import { Box, Button, FormControl, InputAdornment, Link, Modal, Paper, RadioGroup, TextField, Typography } from '@mui/material'
 import React from 'react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import DoneAllIcon from '@mui/icons-material/DoneAll';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 const schema = z.object({
-    name: z.string().nonempty({ message: 'Name is required' }),
-    type: z.string().nonempty({ message: 'Type is required'}),
-    stock: z.string().nonempty({ message: 'Stock is required'}).regex(/^\d+$/, { message: 'Stock must be a postive number'}),
-    msp: z.string().nonempty({ message: 'MSP is required'}).regex(/^\d+$/, { message: 'MSP must be a postive number'}),
-    askprice: z.string().nonempty({ message: 'Ask Price is required'}).regex(/^\d+$/, { message: 'Ask Price must be a postive number'}),
-    location: z.string().nonempty({ message: 'Location is required'}),
+    name: z.string().nonempty({ message: 'Name is required' }).min(3, { message: 'Name must be atleast 3 characters long'}).max(30, { message: 'Name must be atmost 30 characters long'}),
+    type: z.string().nonempty({ message: 'Type is required'}).min(3, { message: 'Type must be atleast 3 characters long'}).max(30, { message: 'Type must be atmost 30 characters long'}),
+    stock: z.string().nonempty({ message: 'Stock is required'}).regex(/^\d+$/, { message: 'Stock must be a postive number'}).min(1, { message: 'Stock must be atleast 1'}),
+    msp: z.string().nonempty({ message: 'MSP is required'}).regex(/^\d+$/, { message: 'MSP must be a postive number'}).min(1, { message: 'MSP must be atleast 1'}),
+    askprice: z.string().nonempty({ message: 'Ask Price is required'}).regex(/^\d+$/, { message: 'Ask Price must be a postive number'}).min(1, { message: 'Ask Price must be atleast 1'}),
+    location: z.string().nonempty({ message: 'Location is required'}).min(3, { message: 'Location must be atleast 3 characters long'}).max(50, { message: 'Location must be atmost 50 characters long'}),
     pickupOn: z.string().nonempty({ message: 'Pickup Date is required'}),
 })
 
@@ -26,8 +29,12 @@ const page = () => {
     const { register, handleSubmit, formState: { errors } } = useForm<FormFields>({
         resolver: zodResolver(schema),
     })
-
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+    
     const onSubmit = async(data: FormFields) => {
+        handleOpen();
         console.log(data);
     }
 
@@ -119,6 +126,7 @@ const page = () => {
                 error={errors.askprice ? true : false}
                 variant="outlined"
                 fullWidth
+                InputProps={{ endAdornment: ( <InputAdornment position="start"> <Payment /> </InputAdornment> ), }}
                 />
                 {errors.askprice && (
                 <Typography color="error" variant="caption">
@@ -134,6 +142,7 @@ const page = () => {
                 error={errors.location ? true : false}
                 variant="outlined"
                 fullWidth
+                InputProps={{ endAdornment: ( <InputAdornment position="start"> <LocationOnIcon/> </InputAdornment> ), }}
                 />
                 {errors.location && (
                 <Typography color="error" variant="caption">
@@ -160,6 +169,7 @@ const page = () => {
             <Button
                 variant="outlined"
                 className="w-fit"
+                href="/f/listings"
             >
                 CANCEL
             </Button>
@@ -170,6 +180,18 @@ const page = () => {
             >
                 SUBMIT
             </Button>
+            <Modal
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="modal-modal-title"
+              aria-describedby="modal-modal-description"
+            >
+                <Box className=" center-col gap-4 rounded-lg bg-white w-80 min-h-80" style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%) ' }}>
+                    <DoneAllIcon style={{ color: 'green', fontSize: 170 }} />
+                    <Typography variant="body1">Your Order Has Been Placed</Typography>
+                    <Button href='#' variant="contained" endIcon={<ArrowForwardIcon/>}>CHECK DETAILS</Button>
+                </Box>
+            </Modal>
         </Box>
         </Box>
       </Paper>
